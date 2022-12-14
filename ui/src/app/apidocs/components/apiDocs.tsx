3a379983f85a6a57f7efa43f04436668b7737ad9
Isitha Subasinghe <isitha@pipekit.io>
2022-12-14 12:52:03 +1100

    wip

--- ui/src/app/apidocs/components/apiDocs.tsx
+++ ui/src/app/apidocs/components/apiDocs.tsx
@@ -1,16 +1,32 @@
 import {Page} from 'argo-ui';
+import {BadMagic} from 'badmagic';
 import * as React from 'react';
-import SwaggerUI from 'swagger-ui-react';
+import {useEffect, useState} from 'react';
 import 'swagger-ui-react/swagger-ui.css';
 import {uiUrl} from '../../shared/base';
 import {useCollectEvent} from '../../shared/components/use-collect-event';
-
+require('../style.scss');
 export const ApiDocs = () => {
     useCollectEvent('openedApiDocs');
+    const [swagger, setSwagger] = useState(null);
+
+    useEffect(() => {
+        fetch(uiUrl('assets/openapi-spec/swagger.json'))
+            .then(res => res.json())
+            .then(json => {
+                console.log(json);
+                setSwagger(json);
+            })
+            .catch(err => console.log(err));
+    }, []);
     return (
         <Page title='Swagger'>
             <div className='argo-container'>
-                <SwaggerUI url={uiUrl('assets/openapi-spec/swagger.json')} defaultModelExpandDepth={0} />
+                {swagger && (
+                    <BadMagic
+                        workspaces={[{id: 'main', name: 'main', routes: [], openapi: JSON.stringify(swagger), config: {baseUrl: uiUrl('assets/openapi-spec/swagger.json')}}]}
+                    />
+                )}
             </div>
         </Page>
     );
